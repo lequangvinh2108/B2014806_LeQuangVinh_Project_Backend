39b0f7547ec064385e4b8dd78a7b2bca04c54b16
lequangvinh2108 <vinhb2014806@student.ctu.edu.vn>
2023-11-13 20:04:54 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const Router = require("./app/routes/route");
+const ApiError = require("./app/api-error");
+
 
 
 app.use(cors());
@@ -12,6 +14,22 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", Router);
+
+// handle 404 response
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error)
+    // sẽ chuyển về middleware xử lý lỗi này
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 
 module.exports = app;
