f1650b2f84e9d3cafbc2394e7815af35fe36a735
lequangvinh2108 <vinhb2014806@student.ctu.edu.vn>
2023-11-13 20:00:42 +0700

    Dinh nghia cac route

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
+const Router = require("./app/routes/route");
+
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Books Store" });
 });
+
+app.use("/api/products", Router);
+
 
 module.exports = app;
